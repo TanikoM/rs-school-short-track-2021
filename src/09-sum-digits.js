e3639ad4cfb7cd4978db327bc6b0fe1aade13059
Tatiana Malovana <taniko2425@gmail.com>
2021-04-11 23:08:26 +0300

    feat: implement task N9

---
+++ src/09-sum-digits.js
@@ -0,0 +1,21 @@
+/**
+ * Given a number, replace this number with
+ * the sum of its digits until we get to a one digit number.
+ *
+ * @param {Number} n
+ * @return {Number}
+ *
+ * @example
+ * For 100, the result should be 1 (1 + 0 + 0 = 1)
+ * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
+ *
+ */
+function getSumOfDigits(n) {
+  const result = (String(n).split('')).map((x) => Number(x)).reduce((a, b) => a + b);
+  if (result > 9) {
+    return getSumOfDigits(result);
+  }
+  return result;
+}
+
+module.exports = getSumOfDigits;
