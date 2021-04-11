6fefd430856cb1aef14400f448ab5442f6d76ff4
Tatiana Malovana <taniko2425@gmail.com>
2021-04-11 22:07:48 +0300

    fix: fix task N3

--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -11,18 +11,16 @@
  */
 function sortByHeight(arr) {
   const arr1 = [];
-  let res = [];
-
+  let arr2 = [];
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === -1) arr1.push(i);
-    else res.push(arr[i]);
+    else arr2.push(arr[i]);
   }
-
-  res = res.sort((a, b) => a - b);
-  for (let j = 0; j < res.length; j++) {
-    res.splice(arr1[j], 0, -1);
+  arr2 = arr2.sort((a, b) => a - b);
+  for (let j = 0; j < arr1.length; j++) {
+    arr2.splice(arr1[j], 0, -1);
   }
-  return res;
+  return arr2;
 }
 
 module.exports = sortByHeight;
